f89099591da0d150b5cbc7c4db91a7184c9c3fb4
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-04-24 10:57:37 +0200

    Implement loadXmlDocument()

--- expertSystem/src/main/java/com/codecool/expertSystem/XMLParser.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/XMLParser.java
@@ -1,7 +1,18 @@
 package com.codecool.expertSystem;
 
-public abstract class XMLParser {
-    public void loadXmlDocument(String xmlPath) {
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
 
+public abstract class XMLParser {
+    public abstract void parse();
+
+    public void loadXmlDocument(String xmlPath) {
+    	try {
+    		Document document = builder.parse(new FileInputStream(xmlPath));
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
