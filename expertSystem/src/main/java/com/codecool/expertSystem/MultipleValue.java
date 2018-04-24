ee99abce8f7539cc8dae9099d3502534a02c1127
Kubset <kuba1228334@gmail.com>
2018-04-24 22:16:51 +0200

    add all basic methods

--- expertSystem/src/main/java/com/codecool/expertSystem/MultipleValue.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/MultipleValue.java
@@ -5,7 +5,19 @@ import java.util.ArrayList;
 
 class MultipleValue {
 
+    private List<String> pattern;
+    private boolean selectionType;
+
     public void multipleValue(List<String> params, boolean selectionType) {
-        //
+        this.pattern.addAll(params);
+        this.selectionType = selectionType;
+    }
+
+    public List<String> gestInputPattern() {
+        return pattern;
+    }
+
+    public boolean getSelectionType() {
+        return selectionType;
     }
 }
