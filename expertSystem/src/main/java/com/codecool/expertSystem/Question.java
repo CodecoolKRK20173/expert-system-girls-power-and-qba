7962ae0fb460617cf65141d6a9a6265aaf350a8b
Kubset <kuba1228334@gmail.com>
2018-04-24 22:17:57 +0200

    implement all getters and make private fields

--- expertSystem/src/main/java/com/codecool/expertSystem/Question.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/Question.java
@@ -1,23 +1,30 @@
 package com.codecool.expertSystem;
 
 public class Question {
-    public Question(String id, String question, Answer answer) {
 
+    String id;
+    String question;
+    Answer answer;
+
+    public Question(String id, String question, Answer answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
     }
 
     public String getId() {
-        return "";
+        return id;
     }
 
     public String getQuestion() {
-        return "";
+        return question;
     }
 
     public Answer getAnswer() {
-        return new Answer();
+        return answer;
     }
 
     public boolean getEvaluatedAnswer(String input) {
-        return false;
+        return answer.evaluateAnswerByInput(input);
     }
 }
