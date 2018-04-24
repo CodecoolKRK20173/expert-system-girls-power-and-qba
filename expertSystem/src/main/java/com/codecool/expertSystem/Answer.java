3bd36fc74aab0c126f3400dbc63e85fc2fc34ae7
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-04-24 20:46:21 +0200

    Implement evaluateAnswerByInput method

--- expertSystem/src/main/java/com/codecool/expertSystem/Answer.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/Answer.java
@@ -1,13 +1,22 @@
 package com.codecool.expertSystem;
 
+import java.util.*;
+
 class Answer {
+    List<Value> valueList = new ArrayList<>();
 
     public void addValue(Value value) {
-        //
+        this.valueList.add(value);
     }
     
-    public boolean evaluateAnswerByInput(String input) {
-        //
+    public boolean evaluateAnswerByInput(String userInput) {
+        for(Value element : this.valueList) {
+            for(String value : element.getInputPattern()) {
+                if(userInput.equals(value)) {
+                    return element.getSelectionType();
+                }
+            }
+        }
         return false;
     }
 }
