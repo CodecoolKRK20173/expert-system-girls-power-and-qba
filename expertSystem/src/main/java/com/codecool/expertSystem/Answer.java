e5853ff59928f40071055664e6586ec6b00597eb
Kubset <kuba1228334@gmail.com>
2018-04-24 22:16:15 +0200

    implement addValue method

--- expertSystem/src/main/java/com/codecool/expertSystem/Answer.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/Answer.java
@@ -1,9 +1,13 @@
 package com.codecool.expertSystem;
+import java.util.List;
+import java.util.ArrayList;
 
 class Answer {
 
+    List<Value> values = new ArrayList<>();
+
     public void addValue(Value value) {
-        //
+        values.add(value);
     }
     
     public boolean evaluateAnswerByInput(String input) {
