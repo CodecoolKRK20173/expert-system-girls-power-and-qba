7417083c2a9e2be5d065cb655eb1d7b367615970
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-04-24 13:47:40 +0200

    Add return statement in ESProvider

--- expertSystem/src/main/java/com/codecool/expertSystem/ESProvider.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/ESProvider.java
@@ -10,10 +10,12 @@ public class ESProvider {
     }
 
     public boolean getAnswerByQuestion(String questionId) {
+    	return false;
 
     }
 
     public String evaluate() {
-
+    	return " ";
     }
 }
+
