606ada951344bf5b596d198e1cf10ba328148d87
Kubset <kuba1228334@gmail.com>
2018-04-24 22:17:12 +0200

    add all basic methods

--- expertSystem/src/main/java/com/codecool/expertSystem/SingleValue.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/SingleValue.java
@@ -1,8 +1,23 @@
 package com.codecool.expertSystem;
 
-class SingleValue {
+import java.util.List;
+import java.util.ArrayList;
+
+class SingleValue extends Value {
+
+    private List<String> pattern;
+    private boolean selectionType;
 
     public void SingleValue(String param, boolean selectionType) {
-        //
+        this.pattern.add(param);
+        this.selectionType = selectionType;
+    }
+
+    public List<String> gestInputPattern() {
+        return pattern;
+    }
+
+    public boolean getSelectionType() {
+        return selectionType;
     }
 }
