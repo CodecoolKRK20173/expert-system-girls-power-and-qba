afd49630b0606c1efdf728567c2d9f2794fd3906
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-04-25 10:39:31 +0200

    Implement main method in Main.java

--- expertSystem/src/main/java/com/codecool/expertSystem/Main.java
+++ expertSystem/src/main/java/com/codecool/expertSystem/Main.java
@@ -3,7 +3,8 @@ package com.codecool.expertSystem;
 class Main {
 
     public static void main(String[] args) {
-        XMLParser parser = new RuleParser("src/main/java/com/codecool/expertSystem/Question.xml");
-        parser.parse();
+        FactParser factParser = new FactParser();
+        RuleParser ruleParser = new RuleParser("jakis plik  ");
+        ESProvider esProvider = new ESProvider(factParser, ruleParser);
     }
 }
